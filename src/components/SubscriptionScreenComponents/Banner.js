2f487a16d40c92b8d3ff4aa8d4c495f244cb6ca9
Neel <neelnarayanshetty@gmail.com>
2023-01-20 00:03:36 +0530

    banner fix

--- src/components/SubscriptionScreenComponents/Banner.js
+++ src/components/SubscriptionScreenComponents/Banner.js
@@ -5,22 +5,42 @@ import { layout } from "../../constants/layout";
 const Banner = () => {
   return (
     <View style={styles.root}>
-      <View style={{ flex: 3 }}>
-        <View>
+      <View
+        style={{
+          flex: 2,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            flex: 2,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <Text style={styles.text}>
             Enroll your Organization, Agency or Company to get unlimited access
             and register unlimited vehicles, employees, users
           </Text>
         </View>
-        <View>
+        <View style={{ flex: 1 }}>
           <TouchableOpacity>
             <View style={styles.buttonContainer}>
-              <Text>click here</Text>
+              <Text style={styles.buttonText}>click here</Text>
             </View>
           </TouchableOpacity>
         </View>
       </View>
-      <View style={{ flex: 1, justifyContent: "flex-start" }}>
+      <View
+        style={{
+          flex: 1,
+          // backgroundColor: "pink",
+          height: 150,
+          alignItems: "flex-start",
+          justifyContent: "flex-end",
+        }}
+      >
         <Image
           source={require("../../../assets/images/phone.png")}
           style={styles.image}
@@ -37,28 +57,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     flexDirection: "row",
-    height: 200,
+    height: 150,
     width: layout.widthp,
     backgroundColor: "#c471ed",
     borderRadius: 20,
-    paddingHorizontal: 20,
+    paddingLeft: 20,
+    overflow: "hidden",
+    paddingTop: 10,
   },
   buttonContainer: {
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "red",
-    height: 50,
-    width: 150,
+    height: 30,
+    width: 80,
     borderRadius: 25,
   },
   text: {
     fontFamily: "poppins-regular",
-    fontSize: 14,
+    fontSize: 13,
     color: "white",
     // textAlign:'center'
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 120,
+    height: 120,
+    transform: [{ translateX: -25 }, { translateY: -5 }],
+  },
+  buttonText: {
+    fontFamily: "poppins-semibold",
+    color: "white",
+    fontSize: 12,
   },
 });
